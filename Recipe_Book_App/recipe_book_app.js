0c74c362cf03a96e4ba874f3be9121779cb67e83
Lesia Soloviova <solovyova.lesya@gmail.com>
2024-08-02 13:15:18 +0300

    feat: finish Project_3 and update README.md

--- Recipe_Book_App/recipe_book_app.js
+++ Recipe_Book_App/recipe_book_app.js
@@ -1,9 +1,37 @@
 const API_KEY = "4adec9229ebf4530922e68c5aa8df789";
 const recipeListEl=document.getElementById("recipe-list");
 
-function displayRecipes(){
+function displayRecipes(recipes){
+  console.log("recipes",recipes)
+  recipeListEl.innerHTML="";
 
+recipes.forEach((recipe)=>{
+const recipeItemEl = document.createElement("li");
+recipeItemEl.classList.add("recipe-item");
 
+const recipeImageEl = document.createElement("img");
+recipeImageEl.src = recipe.image
+recipeImageEl.alt = recipe.title
+
+const recipeTitleEl = document.createElement("h2");
+recipeTitleEl.innerHTML = recipe.title
+
+const recipeIngridientsEl = document.createElement("p");
+recipeIngridientsEl.innerHTML = `
+ <strong>Ingridients:</strong> ${recipe.extendedIngredients.map(ingredient => ingredient.original).join(", ")}`;
+
+const recipeViewRecipeEl = document.createElement("a");
+recipeViewRecipeEl.innerHTML = 'View Recipe';
+recipeViewRecipeEl.href = recipe.spoonacularSourceUrl
+recipeViewRecipeEl.target = "_blank";
+
+recipeItemEl.appendChild(recipeImageEl);
+recipeItemEl.appendChild(recipeTitleEl);
+recipeItemEl.appendChild(recipeIngridientsEl);
+recipeItemEl.appendChild(recipeViewRecipeEl);
+
+recipeListEl.appendChild(recipeItemEl);
+})
 
 }
 
@@ -26,8 +54,7 @@ async function getRecipes() {
 
 async function init() {
   const recipes = await getRecipes();
-  console.log(recipes);
-  displayRecipes()
+  displayRecipes(recipes)
 }
 
 init();
